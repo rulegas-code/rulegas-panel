84382382883edbb8acb0623a3eca55abe60ab817
hediazh <hediazh@gmail.com>
2021-08-06 19:56:46 -0500

    RDS config added

--- rulegas_console/settings.py
+++ rulegas_console/settings.py
@@ -93,6 +93,19 @@ DATABASES = {
 }
 
 
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+
+
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
